2a150ad37b74b46275a0a55c46631aa5697c64c8
Jader <jadermonsalve9@gmail.com>
2023-10-03 11:43:55 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { SeriesComponent } from './components/series/series.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+const routes: Routes = [
+{ path: '', component: HomeComponent },
+{ path: 'navbar', component: NavbarComponent },
+{ path: 'series', component: SeriesComponent },
+{ path: 'movies', component: MoviesComponent },
+{ path: 'footer', component: FooterComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
